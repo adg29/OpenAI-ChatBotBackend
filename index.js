469d3a205a09722ff00f8cf22590c0864d2f3a0f
ArpitxGit <arpitxdungeon@gmail.com>
2023-08-04 02:21:46 +0530

    JSON format with variables

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const systemMessage = {
     "Pick the place from the coordinate\
     Generate a short story(One or Two liner) connected to this place time combinations\
     Don't use the coordinate in the stories rather the place\
-    Provide the output in a json format with Story name, description, date, time, place, coordinate",
+    Provide the output in a json format with Name, Description, Date, Time, Place, Coordinate",
 };
 
 // API endpoint to receive user messages and get Chatbot responses
