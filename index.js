02f5d33c00edbc7c1fa03cf157016b49b379eb55
ArpitxGit <arpitxdungeon@gmail.com>
2023-08-14 16:42:52 +0530

    added token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const env = require("dotenv");
 const authRoutes = require("./src/routes/authRoutes");
+const chatRoutes = require("./src/routes/chatRoutes");
 
 const app = express();
 
@@ -8,7 +9,7 @@ env.config();
 app.use(express.json());
 
 app.use("/auth", authRoutes);
-app.use("/api/chat", authRoutes);
+app.use("/api/chat", chatRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
