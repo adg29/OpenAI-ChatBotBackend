216494d3d961a4f7afe841ae6e7843a9ba9cf585
ArpitxGit <arpitxdungeon@gmail.com>
2023-08-15 23:05:06 +0530

    updated content

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ app.use(express.json());
 const systemMessage = {
   role: "system",
   content:
-    "Generate a short story(One or Two liner) based on the input\
+    "Generate a short story(One or Two liner) based on the input in a different timeline\
     Don't use the input's name in the generated story's name\
-    Make sure the generated story isn't on the same time and date\
     Make sure the generated story is in a different genre\
-    Make sure the generated story is in a different timeline\
     Provide the output in a json format with Story name, description, date, time, place, coordinate",
 };
 
