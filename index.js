4a5bf3a820d90ad67fa7b3add324bed29e2286d6
ArpitxGit <arpitxdungeon@gmail.com>
2023-12-26 11:50:32 +0530

    upgraded to gpt-4-1106-preview

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const env = require("dotenv");
 const { default: OpenAI } = require("openai");
 
 const app = express();
-
 env.config();
 app.use(express.json());
 
@@ -14,34 +13,38 @@ const openai = new OpenAI({
 const systemMessage = {
   role: "system",
   content:
-    'You are a narrative designer who designs Mise-en-scène and Limerick,\
-    Make sure to generate the Limerick in unsettling tone,\
-    The output will contain a Name, Limerick and Description\
-    The Name should be a title of the limerick that describes what takes place in the limerick\
+    'You are a narrative designer who designs Mise-en-scène and Limerick, \
+    Make sure to generate the Limerick in an unsettling tone, \
+    The output will contain a Name, Limerick, and Description. \
+    The Name should be a title of the limerick that describes what takes place in the limerick. \
     Provide the output in JSON structure like this {"1": "<The name>", "2": "<The Limerick>", "3": "<The Mise-en-scène>"}',
 };
 
 app.post("/api/chat", async (req, res) => {
-  const userMessage = req.body.message;
-
-  const apiMessages = [{ role: "user", content: userMessage }];
-
-  const requestData = {
-    model: "gpt-4-1106-preview",
-    response_format: { type: "json_object" },
-    messages: [systemMessage, ...apiMessages],
-  };
-
   try {
+    const userMessage = req.body.message;
+    const apiMessages = [{ role: "user", content: userMessage }];
+
+    const requestData = {
+      model: "gpt-4-1106-preview",
+      response_format: { type: "json_object" },
+      messages: [systemMessage, ...apiMessages],
+    };
+
     const response = await openai.chat.completions.create(requestData);
     console.log("OpenAI API Call", response);
-    const choices = response.choices;
-    if (response && response.choices && response.choices.length > 0) {
-      const content = JSON.parse(choices[0].message.content) || {};
-      console.log(content);
-    }
 
-    res.status(200).json(content);
+    if (response && response.choices && response.choices.length > 0) {
+      const content = JSON.parse(response.choices[0].message.content) || {};
+      // Remove newline characters and format the output
+      const formattedContent = {};
+      Object.keys(content).forEach((key) => {
+        formattedContent[key] = content[key].replace(/\n/g, " ");
+      });
+      res.status(200).json(formattedContent);
+    } else {
+      throw new Error("No valid response from OpenAI.");
+    }
   } catch (error) {
     console.error("Error:", error.message);
     res.status(500).json({
@@ -52,6 +55,10 @@ app.post("/api/chat", async (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
